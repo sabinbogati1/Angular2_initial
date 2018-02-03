ef4c9d6b0f5be9c66e23aa9948c4752dec6bcb81
sabinbogati <sabin.bogati@convergetree.com>
2018-02-03 23:19:38 +0545

    Broadway Observable completed

--- src/app/authModule/login/login.component.ts
+++ src/app/authModule/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {Router,ActivatedRoute} from "@angular/router"
+import {Router,ActivatedRoute} from "@angular/router";
+//Observable Imports
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-login',
@@ -18,9 +20,104 @@ export class LoginComponent {
 
   }
 
+
+  callForNote(note){
+    return new Promise((resolve,reject)=>{
+      if(note=="ts"){
+        resolve({
+          note:note,
+          desc:"This is note of typescript"
+        })
+      }
+      else{
+        reject("No note");
+      }
+    })
+  }
+
+
+  callForNoteWithObservable(note):Observable<any>{
+    return Observable.create((observer)=>{
+      if(note=="ts"){
+
+        // setTimeout(()=>{
+        //   observer.next({
+        //     note:note,
+        //     desc:"This is note of typescript"
+        //   })
+        // },2000);
+        // setTimeout(()=>{
+        //   observer.next({
+        //     note:note,
+        //     desc:"This is next ........ note of typescript"
+        //   })
+        // },5000);
+
+        // setTimeout(()=>{
+        //   observer.complete();
+        // },6000);
+        let i=0;
+        setInterval(()=>{
+
+          if(i==10){
+            observer.complete();
+          }
+
+          observer.next(i);
+          i++;
+        },1000);
+
+      }
+      else{
+        observer.error("No note");
+      }
+    })
+
+  }
+
   login(){
 
-    this.router.navigate(["/user/dashboard"]);
+    //this.router.navigate(["/user/dashboard"]);
+
+    // this.callForNote("ts")
+    // .then(data=>{
+    //   console.log("Data is here: ", data);
+    // }),
+    // err=>{
+    //   console.log("Error is this " , err);
+    // }
+
+
+    this.callForNoteWithObservable("ts").subscribe(
+      data=>{
+
+        console.log("Data is here at first observable: ", data);
+      },
+      error=>{
+        console.log("Error is at first observable: " , error);
+      },
+      ()=>{
+          console.log("Completed.....");
+      }
+    )
+
+   let a= this.callForNoteWithObservable("ts").subscribe(
+      data=>{
+        if(data==5){
+          a.unsubscribe();
+          console.log("Now I Unsuscribe...")
+        }
+        console.log("Data is here at second  observable: ", data);
+      },
+      error=>{
+        console.log("Error is at observable: " , error);
+      },
+      ()=>{
+          console.log("Completed.....");
+      }
+    )
+
+
   }
 
 
